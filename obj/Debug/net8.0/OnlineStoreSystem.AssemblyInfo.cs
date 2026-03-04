47dd319d3d8714e392b872e872f76acf6bddbf75
Iryna Hrytsenko <ipz244_gis@student.ztu.edu.ua>
2026-03-04 12:37:26 +0200

    Unchanged project

--- obj/Debug/net8.0/OnlineStoreSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/OnlineStoreSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStoreSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3dab235ae74daa858689f4950e30cb924fed639")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a765fd5def3f86a6f15089860690043e3bfcfd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStoreSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStoreSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
